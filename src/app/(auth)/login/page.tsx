11010e305e36daee39290f898fc5594860a14c05
Tim Borovkov <tim.borovkov@icloud.com>
2026-03-26 01:50:34 +0700

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import { getSsoConfig } from "../sso-config";
 import { LoginForm } from "./login-form";
 
 export default function LoginPage() {
   const sso = getSsoConfig();
-  return <LoginForm enableGoogle={sso.google} enableGithub={sso.github} />;
+  return (
+    <Suspense>
+      <LoginForm enableGoogle={sso.google} enableGithub={sso.github} />
+    </Suspense>
+  );
 }
