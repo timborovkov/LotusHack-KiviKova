50e17e5114e6e4dfddc420b1a21d4488b7988cff
Tim Borovkov <tim.borovkov@icloud.com>
2026-03-29 23:47:45 +0700

    Create chat-tab.tsx

---
+++ src/components/meeting/chat-tab.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ChatPanel } from "@/components/chat-panel";
+
+interface ChatTabProps {
+  meetingId: string;
+}
+
+export function ChatTab({ meetingId }: ChatTabProps) {
+  return <ChatPanel meetingId={meetingId} placeholder="Ask about this meeting..." />;
+}
