c2855899336b31a5712deb1540a1b3eb06d6f5cd
Tim Borovkov <tim.borovkov@icloud.com>
2026-04-02 00:11:43 +0700

    Update dialog.tsx

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -147,10 +147,13 @@ function DialogDescription({
 
 export {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
+  DialogOverlay,
+  DialogPortal,
   DialogTitle,
   DialogTrigger,
 };
