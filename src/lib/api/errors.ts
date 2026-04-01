49a90764f7681536405780bbd23420c4a5cb564e
Tim Borovkov <tim.borovkov@icloud.com>
2026-04-02 00:51:00 +0700

    Update errors.ts

--- src/lib/api/errors.ts
+++ src/lib/api/errors.ts
@@ -40,3 +40,10 @@ export class ConflictError extends Error {
     this.name = "ConflictError";
   }
 }
+
+export class SearchError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "SearchError";
+  }
+}
