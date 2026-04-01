72efa2f4114749e6a306e190202cf217394c6ef8
Tim Borovkov <tim.borovkov@icloud.com>
2026-04-01 19:57:09 +0700

    Update types.ts

--- src/lib/blog/types.ts
+++ src/lib/blog/types.ts
@@ -3,7 +3,7 @@ import { z } from "zod/v4";
 export const frontmatterSchema = z.object({
   title: z.string(),
   description: z.string(),
-  date: z.string(),
+  date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
   author: z.string().default("Tim Borovkov"),
   tags: z.array(z.string()).default([]),
   image: z.string().optional(),
